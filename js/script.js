c698e28a695e980f0f71d1162cdb04dfce495f5b
Anna Podsiadlo <annamagdalena.podsiadlo@gmail.com>
2020-11-12 16:20:42 +0100

    Work on articles - titles and htmls

--- js/script.js
+++ js/script.js
@@ -3,33 +3,27 @@ const titleClickHandler = function(event){
     const clickedElement = this;
     console.log('Link was clicked!');
   
-    /* [DONE] remove class 'active' from all article links  */
     const activeLinks = document.querySelectorAll('.titles a.active');
 
     for(let activeLink of activeLinks){
       activeLink.classList.remove('active');
     }
 
-    /* [DONE] add class 'active' to the clicked link */
     console.log('clickedElement:', clickedElement)
     clickedElement.classList.add('active');
   
-    /* [DONE] remove class 'active' from all articles */
     const activeArticles = document.querySelectorAll('.posts article.active');
 
     for(let activeArticle of activeArticles){
       activeArticle.classList.remove('active');
     }
 
-    /*[DONE] get 'href' attribute from the clicked link */
     const articleSelector = clickedElement.getAttribute('href');
     console.log(articleSelector);
 
-    /* [DONE] find the correct article using the selector (value of 'href' attribute) */
     const targetArticle = document.querySelector(articleSelector);
     console.log(targetArticle);
 
-    /* [DONE] add class 'active' to the correct article */
     targetArticle.classList.add('active');
   }
   
@@ -49,11 +43,15 @@ const titleClickHandler = function(event){
     const titleList = document.querySelector(optTitleListSelector);
     titleList.innerHTML = "";
 
-    
-  /* for each article */
-
+    /* for each article */
+    const articles = document.querySelectorAll(optArticleSelector + customSelector);
     /* get the article id */
-
+    for (let article of articles) {
+      const articleId = article.getAttribute('id');
+      const articleTitle = article.querySelector(optTitleSelector).innerHTML;
+      const linkHTML = '<li><a href="#' + articleId + '"><span>' + articleTitle + '</span></a></li>';
+      titleList.innerHTML = titleList.innerHTML + linkHTML;
+    }
     /* find the title element */
 
     /* get the title from the title element */
