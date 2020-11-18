15e31e1013a7800af378069f78723adfb9257c8f
Anna Podsiadlo <annamagdalena.podsiadlo@gmail.com>
2020-11-18 18:33:10 +0100

    Add template and new opt

--- js/script.js
+++ js/script.js
@@ -30,7 +30,8 @@
 
   const optArticleSelector = '.post',
     optTitleSelector = '.post-title',
-    optTitleListSelector = '.titles';
+    optTitleListSelector = '.titles',
+    optArticleTagsSelector = '.post-tags .list';
 
   // eslint-disable-next-line no-inner-declarations
   function generateTitleLinks(){
@@ -57,3 +58,31 @@
   generateTitleLinks();
 
 }
+
+function generateTags(){
+  /* find all articles */
+
+  /* START LOOP: for every article: */
+
+   /* find tags wrapper */
+
+   /* make html variable with empty string */
+
+   /* get tags from data-tags attribute */
+
+   /* split tags into array */
+
+   /* START LOOP: for each tag */
+
+     /* generate HTML of the link */
+
+     /* add generated code to html variable */
+
+   /* END LOOP: for each tag */
+
+  /* insert HTML of all the links into the tags wrapper */
+
+  /* END LOOP: for every article: */
+}
+
+generateTags();
